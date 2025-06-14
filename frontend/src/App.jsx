0c8abc02e475f83f2a4b26c6058b3095ced1321d
davepandit <debajyotipandit8@gmail.com>
2025-06-14 22:25:30 +0530

    Installed flowbite-react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { Button } from "flowbite-react";
 
 function App() {
   return (
     <>
       <div>hi there dave here </div>
+      <Button pill size="sm" className="hover:cursor-pointer">
+        Click me
+      </Button>
     </>
   );
 }
