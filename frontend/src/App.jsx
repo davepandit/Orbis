ed752b8217f81d03ef3fc487ee076a73bbc7ada7
davepandit <debajyotipandit8@gmail.com>
2025-06-14 22:33:27 +0530

    Testing flowbite react with a button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <>
       <div>hi there dave here </div>
-      <Button pill size="sm" className="hover:cursor-pointer">
+      <Button
+        pill
+        size="sm"
+        className="hover:cursor-pointer active:ring-0 focus:ring-0"
+      >
         Click me
       </Button>
     </>
