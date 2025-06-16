b44d4e482153b379ab38bac40bf9cc23712e9edf
davepandit <debajyotipandit8@gmail.com>
2025-06-17 01:29:24 +0530

    React redux setup with RTK query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,17 @@ import App from "./App.jsx";
 import { ThemeProvider } from "flowbite-react";
 import { customTheme } from "./utils/FlowbiteThemes.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={customTheme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
   </StrictMode>
 );
